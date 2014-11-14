0f3e3ec81d46ba58f3d577be7d288cfb8e95351f
Norman Sander <norman@nsander.de>
2014-11-14 19:35:47 +0100

    go

---
+++ HOUtils.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+@interface HOUtils : NSObject
+
++ (void)removeAllChildsOf:view;
++ (UIColor*)UIColorFromRGB:(int)rgbValue withAlpha:(float)alpha;
++ (NSInteger)daysBetweenDate:(NSDate*)fromDateTime andDate:(NSDate*)toDateTime;
++ (void)resetDefaults;
++ (NSDate *)combineDate:(NSDate *)date withTime:(NSDate *)time;
++ (BOOL)date:(NSDate*)date isBetweenDate:(NSDate*)beginDate andDate:(NSDate*)endDate;
++ (void)logFonts;
++ (NSDate *) dateWithBaseDate: (NSDate *) baseDate withCalendar: (NSCalendar *)calendar;
+
+@end
---
+++ HOUtils.m
@@ -0,0 +1,92 @@
+#import "HOUtils.h"
+
+@implementation HOUtils
+
++ (void)removeAllChildsOf:(UIView*)view
+{
+     [[view subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
+}
+
++ (UIColor*)UIColorFromRGB:(int)rgbValue withAlpha:(float)alpha
+{
+    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:alpha];
+}
+
++ (NSInteger)daysBetweenDate:(NSDate*)fromDateTime andDate:(NSDate*)toDateTime
+{
+    NSDate *fromDate;
+    NSDate *toDate;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    [calendar rangeOfUnit:NSDayCalendarUnit startDate:&fromDate
+                 interval:NULL forDate:fromDateTime];
+    [calendar rangeOfUnit:NSDayCalendarUnit startDate:&toDate
+                 interval:NULL forDate:toDateTime];
+    
+    NSDateComponents *difference = [calendar components:NSDayCalendarUnit fromDate:fromDate toDate:toDate options:0];
+    
+    return [difference day];
+}
+
++ (void)resetDefaults {
+    NSUserDefaults * defs = [NSUserDefaults standardUserDefaults];
+    NSDictionary * dict = [defs dictionaryRepresentation];
+    for (id key in dict) {
+        [defs removeObjectForKey:key];
+    }
+    [defs synchronize];
+}
+
++ (NSDate *)combineDate:(NSDate *)date withTime:(NSDate *)time {
+    
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    
+    unsigned unitFlagsDate = NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
+    NSDateComponents *dateComponents = [gregorian components:unitFlagsDate fromDate:date];
+    unsigned unitFlagsTime = NSHourCalendarUnit | NSMinuteCalendarUnit |  NSSecondCalendarUnit;
+    NSDateComponents *timeComponents = [gregorian components:unitFlagsTime fromDate:time];
+    
+   // [dateComponents setSecond:[timeComponents second]];
+    [dateComponents setSecond:0];
+    [dateComponents setHour:[timeComponents hour]];
+    [dateComponents setMinute:[timeComponents minute]];
+    
+    NSDate *combDate = [gregorian dateFromComponents:dateComponents];
+    
+    return combDate;
+}
+
++ (BOOL)date:(NSDate*)date isBetweenDate:(NSDate*)beginDate andDate:(NSDate*)endDate
+{
+    if ([date compare:beginDate] == NSOrderedAscending)
+    	return NO;
+    
+    if ([date compare:endDate] == NSOrderedDescending)
+    	return NO;
+    
+    return YES;
+}
+
++ (void)logFonts
+{
+    for (NSString* family in [UIFont familyNames])
+    {
+        NSLog(@"%@", family);
+        
+        for (NSString* name in [UIFont fontNamesForFamilyName: family])
+        {
+            NSLog(@"  %@", name);
+        }
+    }
+}
+
++ (NSDate *) dateWithBaseDate: (NSDate *) baseDate withCalendar: (NSCalendar *)calendar
+{
+    NSDateComponents *components = [calendar components:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit
+                                               fromDate:baseDate];
+    return [calendar dateFromComponents:components];
+}
+
+
+@end
